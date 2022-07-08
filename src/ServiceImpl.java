257b0a98c923cfa24f971833373169265f56f511
Klara <azimovaklara18@gmail.com>
2022-07-08 05:09:17 +0000

    methods

---
+++ src/ServiceImpl.java
@@ -0,0 +1,16 @@
+public class ServiceImpl implements Servise{
+    @Override
+    public void changeDriver() {
+
+    }
+
+    @Override
+    public void startDriving() {
+
+    }
+
+    @Override
+    public void startRepair() {
+
+    }
+}
