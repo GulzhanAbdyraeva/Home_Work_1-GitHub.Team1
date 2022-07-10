595ef97ef2fce707a59ee244c7f6587e4eae6b08
user <imaginejuli@gmail.com>
2022-07-10 15:25:48 +0300

    added json

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
-import com.google.gson.JsonObject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
+    public static final GsonBuilder GSON_BUILDER = new GsonBuilder();
+    private static final Gson GSON = GSON_BUILDER.setPrettyPrinting().create();
+    public static final Path PATH1 = Paths.get("./json.json");
     public static void main(String[] args) {
+        Driver driver = new Driver();
+        Truck[] trucks = {
+         new Truck(1,"Renault Magnum",driver,State.BASE),
+         new Truck(2,"Volvo",driver,State.BASE),
+         new  Truck(3,"DAF XT",driver,State.BASE),
+        };
+        String json = GSON.toJson(trucks);
+        System.out.println(json);
+        Truck.write(json);
+
     }
+
 }
--- src/com/company/Truck.java
+++ src/com/company/Truck.java
@@ -3,12 +3,25 @@ package com.company;
 import com.company.Driver;
 import com.company.State;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Truck {
+    private static final Path WRITE_PATH = Paths.get("./json.json");
     private int id;
     private String name;
     private Driver driver;
     private State state;
 
+    public Truck(int id, String name, Driver driver, State state) {
+        this.id = id;
+        this.name = name;
+        this.driver = driver;
+        this.state = state;
+    }
 
     public int getId() {
         return id;
@@ -41,6 +54,16 @@ public class Truck {
         this.state = state;
     }
 
+    public static void write(String object){
+        Path write = Paths.get(String.valueOf(WRITE_PATH));
+        try{
+            Files.writeString(write,object, StandardOpenOption.CREATE,StandardOpenOption.WRITE);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     @Override
     public String toString() {
         return "Track{" +
@@ -50,4 +73,5 @@ public class Truck {
                 ", state=" + state +
                 '}';
     }
+//
 }
