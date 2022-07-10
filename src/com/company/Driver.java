88ded443662a71f82022557892f0ef511c0992f7
user <imaginejuli@gmail.com>
2022-07-10 15:51:24 +0300

    added json

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -4,6 +4,14 @@ public class Driver {
     private int id;
     private String name;
 
+    public Driver() {
+    }
+
+    public Driver(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static final GsonBuilder GSON_BUILDER = new GsonBuilder();
     private static final Gson GSON = GSON_BUILDER.setPrettyPrinting().create();
     public static final Path PATH1 = Paths.get("./json.json");
+    private static final Path WRITE_PATH = Paths.get("json.drivers");
     public static void main(String[] args) {
         Driver driver = new Driver();
         Truck[] trucks = {
@@ -22,7 +26,23 @@ public class Main {
         String json = GSON.toJson(trucks);
         System.out.println(json);
         Truck.write(json);
+        Driver[] drivers ={
+                new Driver(1,"Petr"),
+                new Driver(2,"Askar"),
+                new Driver(3,"Uson"),
+        };
+        String json2 = GSON.toJson(drivers);
+        System.out.println(json2);
 
+    }
+
+    public static void write(String obj){
+        Path write = Paths.get(String.valueOf(WRITE_PATH));
+        try{
+            Files.writeString(write,obj, StandardOpenOption.CREATE,StandardOpenOption.WRITE);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
